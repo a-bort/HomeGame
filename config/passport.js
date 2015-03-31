d6069a1efaddcd5063e6301652487290a8f9b655
a-bort <bortron5000@gmail.com>
2015-03-31 14:02:55 -0400

    Better styling

--- config/passport.js
+++ config/passport.js
@@ -8,13 +8,11 @@ module.exports = function(passport){
 
 	// used to serialize the user for the session
     passport.serializeUser(function(user, done) {
-		console.log('serialize');
         done(null, user.id);
     });
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-	    console.log('deserialize');
         User.findOne({id: id}, function(err, user) {
             done(err, user);
         });
@@ -33,25 +31,20 @@ module.exports = function(passport){
 
 			// asynchronous
 			process.nextTick(function() {
-			    console.log('Next tick');
 				// find the user in the database based on their facebook id
 				User.findOne({ 'id' : profile.id }, function(err, user) {
 
 					// if there is an error, stop everything and return that
 					// ie an error connecting to the database
-					console.log('Finding user');
 					if (err){
-						console.log('error finding user');
 						return done(err);
 					}
 
 					// if the user is found, then log them in
 					if (user) {
-						console.log('found user');
 						return done(null, user); // user found, return that user
 					} else {
 						// if there is no user found with that facebook id, create them
-						console.log('creating new user');
 						var newUser            = new User();
 
 						// set all of the facebook information in our user model
@@ -61,13 +54,10 @@ module.exports = function(passport){
 						newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
 						// save our user to the database
-						console.log('saving new user');
 						newUser.save(function(err) {
 							if (err){
-								console.log('error saving new user');
 							   throw err;
 							}
-							console.log('new user saved');
 							// if successful, return the new user
 							return done(null, newUser);
 						});
