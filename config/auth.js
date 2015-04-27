74933a6cca6af95f9456f413b9b76a7b6040cb6e
a-bort <bortron5000@gmail.com>
2015-04-26 21:01:44 -0400

    view and fb auth changes

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1554131611521512', // your App ID
         'clientSecret'  : 'a93569788567937dccfe3d6a25ee9ebf', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'https://pure-earth-4942.herokuapp.com//auth/facebook/callback'
     },
 
 };
