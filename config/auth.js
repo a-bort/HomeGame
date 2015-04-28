2ce515b0fd43ee465bbbe49e0460adae80832ac7
a-bort <bortron5000@gmail.com>
2015-04-28 12:44:14 -0400

    Redirect to http

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1554131611521512', // your App ID
         'clientSecret'  : 'a93569788567937dccfe3d6a25ee9ebf', // your App Secret
-        'callbackURL'   : 'https://homegame.a-bort.com/auth/facebook/callback'
+        'callbackURL'   : 'http://homegame.a-bort.com/auth/facebook/callback'
     },
 
 };
