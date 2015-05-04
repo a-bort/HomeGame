ed6da847cb1458937b924393499f249b8a271572
a-bort <bortron5000@gmail.com>
2015-05-04 14:22:56 -0400

    Attempting to setup config vars

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-var dev = false;
+var dev = true;
 
 exports.fbCallback = dev ? 'http://localhost:3000/auth/facebook/callback' : 'http://homegame.a-bort.com/auth/facebook/callback';
 
 exports.dbUri = dev ? 'localhost/homegame' : 'mongodb://homegame_user:tensfull0fsevens@ds053877.mongolab.com:53877/heroku_app36004460';
 
-exports.port = 3000;
+exports.port = process.env.PORT || 3000;
 
 exports.passportSecret = 'tensfull0fsevens';
