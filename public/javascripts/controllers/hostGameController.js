c26a507cfffccac3ae40908515c157235846625f
Andrew Bort <bortron5000@gmail.com>
2015-06-07 21:43:54 -0400

    Styling for textAngular and default HTML and Subject

--- public/javascripts/controllers/hostGameController.js
+++ public/javascripts/controllers/hostGameController.js
@@ -11,12 +11,41 @@ homeGameApp.controller('HostGameController', function($scope, $http, $location){
     time: '',
     notes: ''
   };
-  
+
   $scope.seatHost = true;
   $scope.emailEnabled = false;
   $scope.subject = "";
   $scope.html = "";
-  
+
+  var defaultSubject = 'Poker Game - %date% at %time%';
+  var defaultHtml = '<p>I\'ll be hosting a poker game on <b>%date%</b> at <b>%time%</b>. %notes%&nbsp;There are only %seats% seats available, so sign up now!</p><p><br></p><p><a name="join-link" href="http://homegame.a-bort.com/join/" target="">Click this link</a> to join immediately*&nbsp;</p><p><span><a name="page-link" href="http://homegame.a-bort.com/join/" target="">Click this link</a> to view the game page.</span><br></p><p><span><br></span></p><p><span style="font-size: 10px">*If this is your first time signing up with Home Game, you will be prompted to login with your facebook account, and authorize the application to access your public info. Don\'t worry, we do not capture or store any of your facebook login information, just your name and email address.&nbsp;</span></p>';
+
+  $scope.populate = function(){
+      $scope.generateHtml();
+      $scope.generateSubject();
+  };
+
+  $scope.generateHtml = function(){
+    $scope.html = replaceDefaultCharacters(defaultHtml);
+  };
+
+  $scope.generateSubject = function(){
+    $scope.subject = replaceDefaultCharacters(defaultSubject);
+  };
+
+  var replaceDefaultCharacters = function(text){
+    for(var key in $scope.dataModel){
+      var val = $scope.dataModel[key];
+      if(key == "date" && val){
+        val = util.formatDateString(val);
+      } else if(key == "time" && val){
+        val = util.formatTimeString(val);
+      }
+      text = text.replace("%" + key + "%", val || "[" + key.toUpperCase() + "]");
+    }
+    return text;
+  };
+
   $scope.gameIsNew = function(){
     return !$scope.dataModel._id;
   }
@@ -25,24 +54,24 @@ homeGameApp.controller('HostGameController', function($scope, $http, $location){
     if(game){
       game.date = new Date(game.date);
       game.time = new Date(game.time);
-    
+
       util.mapSourceToTarget(game, $scope.dataModel);
     }
   }
-  
+
   $scope.save = function(valid){
     if(!valid){
       util.alert('Please complete form');
       return;
     }
-    
+
     var extraOptions = {
       seatHost: $scope.seatHost,
       emailEnabled: $scope.emailEnabled,
       subject: $scope.subject,
       html: $scope.html
     };
-    
+
     $http.post('/host/saveGame', {dataModel: $scope.dataModel, extraOptions: extraOptions}).success(function(data){
       if(data.error){
         util.log(err);
--- public/javascripts/shared/util.js
+++ public/javascripts/shared/util.js
@@ -1,30 +1,30 @@
 function util(){
     var util = this;
-    
+
     var suppressLog = false;
-    
+
     util.log = function(msg){
         if(suppressLog) return;
         console.log(msg);
     };
-    
+
     util.logError = function(msg){
         if(suppressLog) return;
         util.log(msg);
     }
-    
+
     util.alert = function(msg){
         alert(msg);
     }
-    
+
     util.sc = function(controller){
         return angular.element("[ng-controller='" + controller + "Controller']").scope();
     }
-    
+
     util.urlBase = function(){
       return window.location.host;
     }
-    
+
     util.mapSourceToTarget = function(source, target){
       for(var key in source){
         if(target.hasOwnProperty(key)){
@@ -32,9 +32,26 @@ function util(){
         }
       }
     }
-    
+
     util.deepCopy = function(obj){
       return $.extend(true, {}, obj);
+    }
+
+    util.formatDateString = function(dateString){
+      var date = new Date(dateString);
+      return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+    }
+
+    util.formatTimeString = function(timeString) {
+      var time = new Date(timeString);
+      var hours = time.getHours();
+      var minutes = time.getMinutes();
+      var ampm = hours >= 12 ? 'PM' : 'AM';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+ minutes : minutes;
+      var strTime = hours + ':' + minutes + ' ' + ampm;
+      return strTime;
     }
 }
 var util = new util();
