50743e69df21ab4f7d1cc92efcb63461f3e72d43
a-bort <bortron5000@gmail.com>
2015-08-31 22:55:14 -0400

    UI and starting on the player pool page

--- public/javascripts/controllers/playerPoolController.js
+++ public/javascripts/controllers/playerPoolController.js
@@ -1,11 +1,24 @@
 homeGameApp.controller('PlayerPoolController', function($scope, $http, $location){
   $scope.playerPool = [];
-  
+
   $scope.init = function(playerPool){
     $scope.playerPool = playerPool || [];
-  }
-  
+  };
+
   $scope.parsePlayerName = function(player){
     return player.user.customName || player.user.name;
-  }
+  };
+
+  $scope.pendingPlayer = function(player){
+    return !player.blocked && !player.confirmed;
+  };
+
+  $scope.activePlayer = function(player){
+    return player.confirmed && !player.blocked;
+  };
+
+  $scope.blockedPlayer = function(player){
+    return !player.confirmed && player.blocked;
+  };
+
 });
