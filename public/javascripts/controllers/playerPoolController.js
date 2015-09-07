e8e2b9c340011134f7e7f3d6100e5554c155b1bb
a-bort <bortron5000@gmail.com>
2015-09-07 19:11:02 -0400

    Minor player pool changes

--- public/javascripts/controllers/playerPoolController.js
+++ public/javascripts/controllers/playerPoolController.js
@@ -6,7 +6,7 @@ homeGameApp.controller('PlayerPoolController', function($scope, $http, $location
   };
 
   $scope.parsePlayerName = function(player){
-    return player.user.customName || player.user.name;
+    return (player.user.customName || player.user.name) + ' (' + player.user.email + ')';
   };
 
   $scope.pendingPlayer = function(player){
