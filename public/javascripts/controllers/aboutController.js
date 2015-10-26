716b0b6adfa5831b04f1c9e942ce767a9d680eb0
a-bort <bortron5000@gmail.com>
2015-10-25 22:56:16 -0400

    Styling cleanup

--- public/javascripts/controllers/aboutController.js
+++ public/javascripts/controllers/aboutController.js
@@ -2,21 +2,19 @@ homeGameApp.controller('AboutController', function($scope, $http, $location){
 
     $scope.features = [
       {title: 'Player Pool Management', description: ''},
-      {title: 'Game Templates', description: ''},
-      {title: 'Email Integration', description: ''},
       {title: 'Player Groups', description: ''},
-      {title: 'Game Schedule + Auto Notifications', description: ''}
+      {title: 'Game Schedule', description: ''}
     ];
-    
+
     $scope.issues = [
-      {title: 'None! lol', description: ''}
+      {title: 'None! (lol)', description: ''}
     ];
-    
+
     $scope.feedback = "";
-    
+
     $scope.submit = function(){
       if(!$scope.feedback) return;
-      
+
       $http.post('/feedback', {feedback: $scope.feedback}).success(function(data){
         if(data.error){
           util.log(err);
@@ -30,15 +28,15 @@ homeGameApp.controller('AboutController', function($scope, $http, $location){
         util.alert('Error submitting feedback. Guess there\'s no way to submit that issue... ');
       });
     }
-    
+
     $scope.submittedAnimation = function(){
       $("#submit-button").addClass("button-success");
       $("#submit-button").text("Submitted");
-      
+
       window.setTimeout(function(){
         $("#submit-button").removeClass("button-success");
         $("#submit-button").text("Submit");
       }, 3000);
-      
+
     }
 });
